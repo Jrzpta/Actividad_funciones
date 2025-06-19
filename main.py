a0ac9b00f6a3be781883775a9a061e56c644491c
unknown <CETECOM@AOAWSC03LC0510.duoc.acad>
2025-06-18 21:38:09 -0400

    feat/Zapata:Agrego funciones

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
 import os
 from functions import *
 os.system ("cls")
+
+lista = []
+
+while True:
+    opcion = int(input("Ingresa una opcion \n1)agregar contacto\2)Mostrar\n"))
+    if opcion == 1:
+        agregar_nombre(lista)
+    elif opcion == 2:
+        mostrar_notas(lista)
